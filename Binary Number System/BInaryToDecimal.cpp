e4ae742c9a99b196a9a92e60132d852709df9a02
Krrishbalana <krrishbalana@gmail.com>
2025-04-05 19:05:34 +0530

    Wap to print decimal number of binary form

---
+++ Binary Number System/BInaryToDecimal.cpp
@@ -0,0 +1,23 @@
+// convert binary number to Decimal number system
+#include<iostream>
+using namespace std;
+//define function to convert binary number to decimal number
+int decimalNum(int binNum){
+    int pow = 1; // 2^0 = 1
+    int ans = 0;
+    while (binNum > 0)
+    {
+        int rem = binNum % 10;
+        ans += (rem * pow);
+
+        binNum = binNum / 10;
+        pow *= 2;
+
+    }
+    return ans;
+}
+
+int main(){
+    int binNum = 110101;
+    cout << decimalNum(binNum) << endl;
+}
