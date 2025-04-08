7e4a63c41e49e795f018b81d2e892d6facbe0af4
Krrishbalana <krrishbalana@gmail.com>
2025-04-08 15:39:48 +0530

    second largest element in an array

---
+++ Arrays/Second_largest_element.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[] = {3,4,2,5,7,3};
+    int size = 6;
+    int first = INT_MIN;
+    int second = INT_MIN;
+
+    for(int i = 0; i < size; i++){
+        if(arr[i] > first){
+            second = first;
+            first = arr[i];
+        }else if (second < arr[i] && arr[i] != first)
+        {
+            second = arr[i];
+        }
+    }
+
+    if(second == INT_MIN){
+        cout << -1 << endl;
+    }else{
+        cout << "second largest no in array = " << second << endl;
+    }
+}
