ab910cfcc6f27708fd8d9a488a44ffd5b780d09c
Krrishbalana <krrishbalana@gmail.com>
2025-04-10 23:47:20 +0530

    bitwise right shift operator

---
+++ Bitwiseoperators_datatypemodifiers/rightshift.cpp
@@ -0,0 +1,12 @@
+//Bitwise >>(right shift) operator --> works on binary numbers;
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 4;    // binary form-- 100
+    int b = 8;    // binary form-- 1000
+
+    cout << (a >> 1) << endl;
+
+    return 0;
+
+}
