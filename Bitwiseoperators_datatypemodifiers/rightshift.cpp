da081f42569252ff7aa85fdad2896fafc396e295
Krrishbalana <krrishbalana@gmail.com>
2025-04-11 22:31:10 +0530

    adding comments in rightshift file

--- Bitwiseoperators_datatypemodifiers/rightshift.cpp
+++ Bitwiseoperators_datatypemodifiers/rightshift.cpp
@@ -1,9 +1,10 @@
 //Bitwise >>(right shift) operator --> works on binary numbers;
+// it gives ans = a/b^2
 #include<iostream>
 using namespace std;
 int main(){
     int a = 4;    // binary form-- 100
-    int b = 8;    // binary form-- 1000
+    int b = 1;    // binary form-- 1000
 
     cout << (a >> 1) << endl;
 
