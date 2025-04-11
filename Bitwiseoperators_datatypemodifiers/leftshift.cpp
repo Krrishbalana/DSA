613680b4287ffbf5824202ca8afd7dd9d4d62efd
Krrishbalana <krrishbalana@gmail.com>
2025-04-11 22:30:21 +0530

    adding comments in leftshift file

--- Bitwiseoperators_datatypemodifiers/leftshift.cpp
+++ Bitwiseoperators_datatypemodifiers/leftshift.cpp
@@ -1,11 +1,12 @@
 //Bitwise <<(left shift operator) operator --> works on binary numbers;
+// it gives ans = a * 2^b
 #include<iostream>
 using namespace std;
 int main(){
     int a = 4;    // binary form-- 100
-    int b = 8;    // binary form-- 1000
+    int b = 1;    // binary form-- 1000
 
-    cout << (a << 1) << endl;
+    cout << (a << b) << endl;
 
     return 0;
 
