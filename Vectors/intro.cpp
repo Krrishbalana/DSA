7bf59324993cedb8dceed97252dc17e97dcf616c
Krrishbalana <krrishbalana@gmail.com>
2025-04-12 18:42:45 +0530

    introduction of vectors

---
+++ Vectors/intro.cpp
@@ -0,0 +1,14 @@
+// Vectors are just like arrays but the major difference between arrays and vectors
+// is that vector is dynamic in nature (size of vector is not fixed).
+// To use vector DS we need to import STL (Standard Template Library).
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> vecA; // size of this vector is 0
+    vector<int> vecB = {1, 2, 3};
+
+    // cout << vecA[0] << endl; // this gives us segmentation fault (undefined behavior)
+    cout << vecB[0] << endl; // this gives us value "1".
+}
