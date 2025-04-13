bd2b578b8d47b1cd95ca7afe69b48cc37168860b
Krrishbalana <krrishbalana@gmail.com>
2025-04-13 18:48:01 +0530

    adding one more example of for each loop

--- Vectors/for_each_loop.cpp
+++ Vectors/for_each_loop.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main(){
     vector<int> vec = {11,22,33};
+    vector<char> vecA = {'a', 'b', 'c'};
     for(int i : vec){
         cout << i << endl; // data type of iterator(i) = datatype of vector, and this i stor the value present in vector
+    }
+    for(char val : vecA){
+        cout << val << endl;
     }
 }
