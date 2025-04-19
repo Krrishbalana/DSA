8754dec9ee9092e99dc0edfc6f5fcb534bbdf2fb
Krrishbalana <krrishbalana@gmail.com>
2025-04-19 23:44:40 +0530

    brute force approach to solve majority sum problem

---
+++ leactue11(majoritysum)/maj_ele.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int majorityele(vector<int> nums){
+    int n = nums.size();
+
+    for(int val : nums){
+        int freq = 0;
+        for(int el : nums){
+            if(el == val){
+                freq ++;
+            }
+        }
+        if (freq > n/2)
+        {
+            return val;
+        }
+        
+    }
+    return -1;
+}
+
+
+int main(){
+    vector<int> nums = {2, 2, 1, 1, 1};
+
+    cout << majorityele(nums) << endl;
+}
