81d9e243215b97d58ab893970a54ab7a62aa36d1
Krrishbalana <krrishbalana@gmail.com>
2025-04-19 23:55:37 +0530

    moore's algo

---
+++ leactue11(majoritysum)/ Moores_Voting_Algorithm.cpp
@@ -0,0 +1,13 @@
+// this is the most optimised approach for majority sum problem
+#include<iostream>
+#include<vector>
+using namespace std;
+int majority(vector<int> nums){
+    int n = nums.size();
+}
+
+int main(){
+    vector<int> nums = {1,2,2,1,1};
+    cout << majority << endl;
+
+}
