bcecbcb291a9de07ef87f3c0d40b8ce41a1a49ca
Krrishbalana <krrishbalana@gmail.com>
2025-05-21 20:47:05 +0530

    print address of variable

---
+++ Binary Search/address.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    cout << &a << endl;  // this gives an hexadecimal value with reffers to the location where a in stored in the memory of PC
+
+    return 0;
+}
