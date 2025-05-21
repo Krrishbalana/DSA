c714cbe26a76faab87f62fc83f3fa8bad558eedc
Krrishbalana <krrishbalana@gmail.com>
2025-05-21 21:33:04 +0530

    binary search algorith

--- Binary Search/01BinarySearch.cpp
+++ Binary Search/01BinarySearch.cpp
@@ -1,19 +1,16 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-int main(){
-    int arr[] = {-1, 0, 3, 4, 5, 9, 12}; // for bianry search array should be sorted
-    int target = 9;
-    int answer = -1;
 
-    //solution
+int BinarySearch(vector<int> arr, int target){
     int st = 0;
-    int end = (sizeof(arr)/sizeof(arr[0])) - 1;
+    int end = arr.size() - 1;
     
     while(st <= end){
         int mid = st + (end-st)/2;
 
         if(arr[mid] == target){
-            mid = answer;
+            return mid;
         }
         else if(arr[mid] > target){
             end = mid - 1;
@@ -21,7 +18,14 @@ int main(){
             st = mid + 1;
         }
     }
+    return -1;
+}
+int main(){
+    vector<int> arr = {-1, 0, 3, 4, 5, 9, 12}; // for bianry search array should be sorted
+    int target = 9;
+    int answer = -1;
 
-    cout << answer << endl;
+   
+    cout << BinarySearch(arr, target) << endl;
 
 }
